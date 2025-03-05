6dc2e404bef3df3c65f2b006b24ee43412b34906
HopeWu <30311646+HopeWu@users.noreply.github.com>
2025-03-05 16:32:55 +0000

    complete editable install

--- docker/init.sh
+++ docker/init.sh
@@ -1,12 +1,12 @@
 #!bin/bash
 
-if [ -d "/home/frappe/frappe-bench/apps/frappe" ]; then
-    echo "Bench already exists, skipping init"
-    cd frappe-bench
-    bench start
-else
-    echo "Creating new bench..."
-fi
+# if [ -d "/home/frappe/frappe-bench/apps/frappe" ]; then
+#     echo "Bench already exists, skipping init"
+#     cd frappe-bench
+#     bench start
+# else
+#     echo "Creating new bench..."
+# fi
 
 export PATH="${NVM_DIR}/versions/node/v${NODE_VERSION_DEVELOP}/bin/:${PATH}"
 
@@ -24,7 +24,8 @@ bench set-redis-socketio-host redis:6379
 sed -i '/redis/d' ./Procfile
 sed -i '/watch/d' ./Procfile
 
-bench get-app lms
+bench get-app lms /repos/lms
+bench get-app payments /repos/payments
 
 bench new-site lms.localhost \
 --force \
@@ -33,6 +34,7 @@ bench new-site lms.localhost \
 --no-mariadb-socket
 
 bench --site lms.localhost install-app lms
+bench --site lms.localhost install-app payments
 bench --site lms.localhost set-config developer_mode 1
 bench --site lms.localhost clear-cache
 bench use lms.localhost
