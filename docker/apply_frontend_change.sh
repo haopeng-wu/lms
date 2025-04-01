01a6813bed5d4e0e142fef5fc882a2c4a2504019
haopeng-wu <haopeng.wu@ul.com>
2025-04-01 17:42:18 +0200

    update apply_frontend_change for the pages to take effect

--- docker/apply_frontend_change.sh
+++ docker/apply_frontend_change.sh
@@ -3,5 +3,6 @@
 
 # copy over the lms front end files
 cp -r /repos/lms/frontend/src/pages /home/frappe/frappe-bench/apps/lms/frontend/src
+cp -r /repos/lms/frontend/src/pages /home/frappe/frappe-bench/frappe-bench/apps/lms/frontend/src/
 cd ~/frappe-bench/apps/lms/frontend
 npm run build
