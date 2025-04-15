dd97bdb09a542ecfc43bd043145cffc4d07af609
haopeng-wu <haopeng.wu@ul.com>
2025-04-15 21:24:37 +0100

    make apply front script more robust

--- docker/apply_frontend_change.sh
+++ docker/apply_frontend_change.sh
@@ -6,3 +6,5 @@ cp -r /repos/lms/frontend/src /home/frappe/frappe-bench/apps/lms/frontend
 cp -r /repos/lms/frontend/src /home/frappe/frappe-bench/frappe-bench/apps/lms/frontend/
 cd ~/frappe-bench/apps/lms/frontend
 npm run build
+cd ~/frappe-bench/frappe-bench/apps/lms/frontend/
+npm run build
